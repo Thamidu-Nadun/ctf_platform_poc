6cdd0fd421ca2a18928698b91a0856f9d7e6596d
thamidu-nadun <nadunrz101@gmail.com>
2026-08-13 23:18:27 +0530

    feat: add DELETE endpoint for instance removal with error handling

--- gateway/service/instance.service.js
+++ gateway/service/instance.service.js
@@ -37,8 +37,8 @@ router.use("/i/:instanceId", (req, res, next) => {
 });
 
 /**
- * POST /i/:challengeId
- * Creates a new instance for the given challengeId.
+ * POST /c/:challengeId
+ * Creates a new instance/container for the given challengeId.
  * If the challengeId is not found, returns a 404 error.
  *
  * @param {string} challengeId - The ID of the challenge to create an instance for.
@@ -77,6 +77,40 @@ router.post("/c/:challengeId", async (req, res) => {
   } catch (error) {
     console.error("Error creating instance:", error);
     res.status(500).send("Error creating instance");
+  }
+});
+
+/**
+ * DELETE /c/:instanceId
+ * Deletes the instance/container with the given instanceId.
+ * If the instanceId is not found, returns a 404 error.
+ *
+ * @param {string} instanceId - The ID of the instance to delete.
+ */
+router.delete("/c/:instanceId", async (req, res) => {
+  const { instanceId } = req.params;
+  if (!instanceId) return res.status(404).send("Instance ID is required!");
+
+  const instance = instances[instanceId];
+  if (!instance) return res.status(404).send("Instance is not found!");
+
+  try {
+    const docker = await getDocker();
+    const containerName = `challenge-${instanceId}`;
+    const container = docker.getContainer(containerName);
+
+    // stop the container
+    await container.stop();
+
+    // remove the container
+    await container.remove();
+
+    delete instances[instanceId];
+
+    res.status(200).send(`Successfully removed the ${containerName}`);
+  } catch (error) {
+    console.log("Error deleting instance", error);
+    res.status(500).send("Error deleting instance");
   }
 });
 
